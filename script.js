09cd453a1b6e640cf0557f04b2429c3914ca3ecd
langroops <langroops@gmail.com>
2022-03-22 13:22:34 -0400

    external games test

--- script.js
+++ script.js
@@ -1,22 +1,79 @@
 const pixels = 49
 const adj = Math.sqrt(pixels)
 
-const games = {
-  "22/03/2022": [9, 16, 11, 18, 28, 36, 37, 38, 39, 40, 34],
-  "23/03/2022": [8, 16, 23, 30, 31, 32, 2, 5, 18, 17, 12, 36, 40],
-  "24/03/2022": [0, 8, 42, 36, 6, 12, 48, 40, 30, 32, 18, 16],
-}
+// const games = {
+//   "22/03/2022": [9, 16, 11, 18, 28, 36, 37, 38, 39, 40, 34],
+//   "23/03/2022": [8, 16, 23, 30, 31, 32, 2, 5, 18, 17, 12, 36, 40],
+//   "24/03/2022": [0, 8, 42, 36, 6, 12, 48, 40, 30, 32, 18, 16],
+// }
+
+import {games} from "games.json";
 
 const date = new Date()
 const today = date.toLocaleDateString('en-GB').toString();
 console.log(games[today]);
 
 const game = games[today];
+
 let selection = [];
 let totalSelected = 0;
+let allCorrect = [];
+let allWrong = [];
+let allAdjacent = [];
 
 let message = document.getElementById("message");
 let guesses = document.getElementById("guesses");
+
+let displayCorrect = document.getElementById("display-correct");
+displayCorrect.addEventListener('click', function(){
+  for(let i = 0; i < pixels; i++){
+    for(let j = 0; j < allCorrect.length; j++){
+      if (i == allCorrect[j]){
+        const pixel = document.getElementById(i);
+        if(pixel.classList.contains("correct-history")){
+          pixel.classList.remove("correct-history")
+        }
+        else{
+        pixel.classList.add("correct-history")
+      }
+      }
+    }
+  }
+})
+let displayWrong = document.getElementById("display-wrong");
+displayWrong.addEventListener('click', function(){
+  for(let i = 0; i < pixels; i++){
+    for(let j = 0; j < allWrong.length; j++){
+      if (i == allWrong[j]){
+        const pixel = document.getElementById(i);
+        if(pixel.classList.contains("wrong-history")){
+          pixel.classList.remove("wrong-history")
+        }
+        else{
+        pixel.classList.add("wrong-history")
+      }
+      }
+    }
+  }
+})
+
+let displayAdjacent = document.getElementById("display-adjacent");
+displayAdjacent.addEventListener('click', function(){
+  for(let i = 0; i < pixels; i++){
+    for(let j = 0; j < allAdjacent.length; j++){
+      if (i == allAdjacent[j]){
+        const pixel = document.getElementById(i);
+        if(pixel.classList.contains("adjacent-history")){
+          pixel.classList.remove("adjacent-history")
+        }
+        else{
+        pixel.classList.add("adjacent-history")
+      }
+      }
+    }
+  }
+})
+
 
 message.innerHTML = "Select " + game.length;
 
@@ -64,19 +121,60 @@ submit.addEventListener('click', function(event) {
   }
   lopp1: for (let i = 0; i < selection.length; i++) {
     loop2: for (let j = 0; j < game.length; j++) {
-      if (selection[i] == game[j]) {
-        const pixel = document.getElementById(selection[i]);
-        pixel.classList.remove("adjacent");
-        pixel.classList.add("correct");
-        guess.push(Number(selection[i]))
-        break loop2;
-      } else if (selection[i] == (game[j] + 1) || selection[i] == (game[j] - 1) || selection[i] == (game[j] + adj) || selection[i] == (game[j] - adj)) {
-        const pixel = document.getElementById(selection[i]);
-        pixel.classList.add("adjacent");
-      }
+      const pixel = document.getElementById(selection[i]);
+      if (!pixel.classList.contains("correct") && !pixel.classList.contains("adjacent") ){
+       pixel.classList.add("wrong");
+     }
+       if (!pixel.classList.contains("correct") && (selection[i] == (game[j] + 1) || selection[i] == (game[j] - 1) || selection[i] == (game[j] + adj) || selection[i] == (game[j] - adj))) {
+         pixel.classList.remove("wrong");
+         pixel.classList.add("adjacent");
+       }
+       if (selection[i] == game[j]) {
+         pixel.classList.remove("wrong");
+         pixel.classList.remove("adjacent");
+         pixel.classList.add("correct");
+         if (!allCorrect.includes(pixel.getAttribute('id'))){
+           allCorrect.push(pixel.getAttribute('id'));
+         }
+       }
+      // if (selection[i] == game[j]) {
+      //   // const pixel = document.getElementById(selection[i]);
+      //   pixel.classList.remove("adjacent");
+      //   pixel.classList.remove("wrong");
+      //   pixel.classList.add("correct");
+      //   guess.push(Number(selection[i]))
 
-    }
+      //   break loop2;
+      // } else {
+      //  if (selection[i] == (game[j] + 1) || selection[i] == (game[j] - 1) || selection[i] == (game[j] + adj) || selection[i] == (game[j] - adj)) {
+      //   // const pixel = document.getElementById(selection[i]);
+      //   pixel.classList.remove("wrong");
+      //   pixel.classList.add("adjacent");
+      // }else{
+      //    pixel.classList.add("wrong");
+      // }
+      // }
+      //
+      }
   }
+  for(let i = 0; i < pixels; i++){
+      const pixel = document.getElementById(i);
+      if(pixel.classList.contains("wrong")){
+        if (!allWrong.includes(pixel.getAttribute('id'))){
+          allWrong.push(pixel.getAttribute('id'));
+        }
+      }
+  }
+
+  for(let i = 0; i < pixels; i++){
+      const pixel = document.getElementById(i);
+      if(pixel.classList.contains("adjacent")){
+        if (!allAdjacent.includes(pixel.getAttribute('id'))){
+          allAdjacent.push(pixel.getAttribute('id'));
+        }
+      }
+  }
+
   let gameOver = false;
   if (selection.length === game.length) {
     let index = 0;
@@ -116,6 +214,11 @@ function reset(guessNum, gameOver) {
   let board = document.getElementById("board");
   let guessBoard = board.cloneNode(true);
   guessBoard.setAttribute('id', 'guess-board-' + guessNum);
+  for(let i = 0; i < guessBoard.children.length; i++){
+     let guessPixel = guessBoard.children[i];
+     guessPixel.setAttribute('id',"guess-" + guessNum + "pixel-" + i)
+     guessPixel.classList.add("guess-pixel")
+  }
   guessBoard.classList.remove("board")
   guessBoard.classList.add("guess-board")
   guesses.appendChild(guessBoard);
